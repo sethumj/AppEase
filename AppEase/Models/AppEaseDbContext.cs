d0bcb6d0023ca934c3b3ad33d11712adaf10726b
sethumj <sethumj001@gmail.com>
2024-06-05 12:15:43 -0400

    Job Model added

--- AppEase/Models/AppEaseDbContext.cs
+++ AppEase/Models/AppEaseDbContext.cs
@@ -4,7 +4,7 @@ namespace AppEase.Models
     public class AppEaseDbContext : DbContext
     {
         public DbSet<Profile> Profiles { get; set; }
-
+        public DbSet<Job> Jobs { get; set; }
         public AppEaseDbContext(DbContextOptions<AppEaseDbContext> options) : base(options) { }
     }
 }
---
+++ AppEase/Models/Job.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace AppEase.Models
+{
+    public class Job
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Job Title required")]
+        public string Title { get; set; }
+        [Required(ErrorMessage ="Job Description Requied")]
+        public string Description { get; set; }
+
+    }
+}
