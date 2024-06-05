05a0b57b36a7709a0b50c3324eebe9e5bff292a5
sethumj <sethumj001@gmail.com>
2024-06-05 12:09:20 -0400

    Add project files.

---
+++ AppEase/Models/AppEaseDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace AppEase.Models
+{
+    public class AppEaseDbContext : DbContext
+    {
+        public DbSet<Profile> Profiles { get; set; }
+
+        public AppEaseDbContext(DbContextOptions<AppEaseDbContext> options) : base(options) { }
+    }
+}
---
+++ AppEase/Models/Profile.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppEase.Models
+{
+    public class Profile
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Name is required")]
+        public string Name { set; get; }
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress(ErrorMessage ="Invalid Email Address")]
+        public string Email { set; get; }
+        public string? LinkedIn { set; get; }
+        public string? Github { set; get; }
+        [Required(ErrorMessage ="Resume is Required")]
+        public string Resume {  set; get; }
+        [Required(ErrorMessage ="Skill Set is Required")]
+        public string SkillSet { set; get; }
+
+
+    }
+}
